08939ce18747b05e309385ba53b4ff5a76c9ec33
Anzchen <Anthonyzchen@yahoo.com>
2023-12-31 00:20:50 -0500

    media queries

--- main.js
+++ main.js
@@ -1,15 +1,55 @@
-import {gsap} from "gsap"
-import imagesLoaded from "imagesloaded"
+import {gsap} from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import imagesLoaded from 'imagesloaded'
 
-var loader = document.getElementById("logo-container");
-const content = document.querySelector("section")
+gsap.registerPlugin(ScrollTrigger) 
+
+const content = document.querySelector('.all-content')
+const slider = document.querySelector('.slider')
+const sections = gsap.utils.toArray('.slider section')
+
+// scroll website back to the top on refresh
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 
 // get all images
 const imgLoad = imagesLoaded(content)
-
+// after images are all gotten, create the timeline
 imgLoad.on('done', instance => {
+  var timeline = gsap.timeline({
+    // allow scrolling after the pre-loader has finished animating
+    onComplete: function() {
+      document.body.classList.remove('no-scroll');
+    }
+  })
+
   // hide icon
-  var timeline = gsap.timeline()
-  timeline.to('svg', {opacity: 0})
-  timeline.to('.blinder', {scaleY: 0, ease: "slow"})
+  timeline
+  .to('svg', {opacity: 0, delay: 1, duration: .5})
+  .to('.blinder', {scaleY: 0, stagger: .3, ease: 'power3.out'})
+})
+
+// find the distance to be scrolled
+function getScrollAmt() {
+  let sliderWidth = slider.scrollWidth;
+  return -(sliderWidth - window.innerWidth)
+}
+
+const tween = gsap.to(slider, {
+  x: getScrollAmt,
+  duration: 3,
+  ease: 'none'
+})
+
+// horizontal scrolling using pin and the animation above
+ScrollTrigger.create({
+  trigger: '.outer',
+  start: 'top',
+  end: () => `+=${getScrollAmt() * -1}`,
+  pin: true,
+  animation: tween,
+  scrub: 1,
+  invalidateOnRefresh: true,
+  markers: true
 })
