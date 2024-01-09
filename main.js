93be5cc45f6055704dc1e82eea2ddffc53118f51
Anzchen <Anthonyzchen@yahoo.com>
2024-01-08 23:51:36 -0500

    header

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import {gsap} from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import imagesLoaded from 'imagesloaded'
+import Lenis from '@studio-freight/lenis'
 
 gsap.registerPlugin(ScrollTrigger) 
 
@@ -8,41 +9,56 @@ const content = document.querySelector('.all-content')
 const slider = document.querySelector('.slider')
 const sections = gsap.utils.toArray('.slider section')
 
-// scroll website back to the top on refresh
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
-
-// get all images
+// Getting all images
 const imgLoad = imagesLoaded(content)
-// after images are all gotten, create the timeline
+
+// Variable to enable / disable lenis
+let lenisEnable = false;
+
+// After images are all gotten, create the timeline
 imgLoad.on('done', instance => {
   var timeline = gsap.timeline({
-    // allow scrolling after the pre-loader has finished animating
+    // Disable scrolling during page load
     onComplete: function() {
       document.body.classList.remove('no-scroll');
+      document.querySelector('.loader').style.zIndex = "0";
+      lenisEnable = true
     }
   })
-
-  // hide icon
+  // Hide icon and begin timeline animations
   timeline
   .to('svg', {opacity: 0, delay: 1, duration: .5})
   .to('.blinder', {scaleY: 0, stagger: .3, ease: 'power3.out'})
 })
 
-// find the distance to be scrolled
+// Lenis Smooth Scrolling
+const lenis = new Lenis()
+
+function raf(time) {
+  if (lenisEnable) {
+    lenis.raf(time)
+  }
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+// Scroll website back to the top on refresh
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+// Finding the distance to be scrolled
 function getScrollAmt() {
   let sliderWidth = slider.scrollWidth;
   return -(sliderWidth - window.innerWidth)
 }
 
 const tween = gsap.to(slider, {
-  x: getScrollAmt,
-  duration: 3,
-  ease: 'none'
+  x: getScrollAmt
 })
 
-// horizontal scrolling using pin and the animation above
+// Horizontal scrolling using pin and the animation above
 ScrollTrigger.create({
   trigger: '.outer',
   start: 'top',
@@ -54,3 +70,29 @@ ScrollTrigger.create({
   markers: true
 })
 
+// Changing background color for each project
+const tween2 = gsap.to('.outer', {
+  backgroundColor: '#739072'
+})
+
+ScrollTrigger.create({
+  trigger: '.project2',
+  start: 'top center',
+  end: 'bottom center',
+  scrub: 1,
+  animation: tween2,
+  markers: true 
+})
+
+// const tween3 = gsap.to('.outer', {
+//   backgroundColor: '#ECE3CE'
+// })
+
+// ScrollTrigger.create({
+//   trigger: '.project2',
+//   start: 'bottom center',
+//   end: '150% center',
+//   scrub: 1,
+//   animation: tween3,
+//   markers: true 
+// })
