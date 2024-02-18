6c3a91279eed46d4ba9db98825e54474b6dd88a6
Anzchen <Anthonyzchen@yahoo.com>
2024-02-18 16:59:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { gsap } from "gsap/dist/gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import imagesLoaded from 'imagesloaded'
 import Lenis from '@studio-freight/lenis'
 
